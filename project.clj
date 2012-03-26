b662118df47756b502a805340ba5baa5a48ff71e
Sam Aaron <samaaron@gmail.com>
2012-03-26 20:12:37 +0100

    bump to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/libs.handlers "0.1.0"
+(defproject overtone/libs.handlers "0.1.1"
   :description "Overtone Event Handler"
   :url "http://github.com/overtone/handlers"
   :dependencies [[org.clojure/clojure "1.3.0"]])
